62bb5505488655eb0ab5c0afdb9f2196c98074e2
Pruet Boonma <pruetboonma@gmail.com>
2015-08-12 22:50:12 +0700

    Initiate homework

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static int process(int i)
+        {
+            int result;
+            if(i > 1)
+            {
+                result = i * process(i - 1);
+            } else
+            {
+                result = 1;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int j = 5;
+            Console.WriteLine(process(j));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework/Homework/Readme.md
@@ -0,0 +1,13 @@
+﻿# Instruction
+* Read the code, understand it, think hard how it works.
+* Change the code such that it will accept a number from command line, then show the result.
+* Answer the questions below
+
+# Questions
+* Why we need "if-else" in function process?
+
+Answer:
+
+
+# Resources
+* https://en.wikipedia.org/wiki/Recursion_(computer_science)
