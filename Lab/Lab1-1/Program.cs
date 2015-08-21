8c95b6ac4fba2cb5c2beb3de800dfe9a66b86761
iamzack13110 <nutthakornzack1@gmail.com>
2015-08-21 14:49:39 +0700

    sort from larger to smaller

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -21,7 +21,7 @@ namespace Lab1_1
                 flag = false;
                 for (int i = 0; i != input.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    if (input[i] < input[i + 1])
                     {
                         int temp = input[i];
                         input[i] = input[i + 1];
